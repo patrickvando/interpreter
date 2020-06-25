e07a571b19a0f64da62d447c7bad2e8e5db18bf1
patrickvando <patrickvando2@gmail.com>
2020-06-25 20:29:03 +0000

    renamed to compiler

--- nrj_compiler.py
+++ nrj_compiler.py
@@ -1,22 +1,17 @@
 import sys
+import os
 from Lexer.Buffer import Buffer
 from Lexer.Lexer import Lexer
 from Parser.Statement_parser import Statement_parser
-from Interpreter.Statement_interpreter import Statement_interpreter
-from Interpreter.Symbol_table import *
 from Compiler.Compiler_main import Compiler_main
 
-def interpret(filename):
+def compile(filename):
     lex  = Lexer(filename)
     stat_parser = Statement_parser(lex)
     stat_list = stat_parser.parse_statement_list()
-    st_stack = Symbol_table_stack()
-    st_stack.push_table()
-    print(stat_list)
-    compiler = Compiler_main("out.asm")
+    head, tail = os.path.split(filename)
+    compiler = Compiler_main(head + ".asm")
     compiler.compile_ast(stat_list)
-    #stat_interpreter = Statement_interpreter(st_stack)
-    #stat_interpreter.interpret_statement_list(stat_list)
 
 arg_len = len(sys.argv)
 if arg_len != 2:
@@ -24,4 +19,4 @@ if arg_len != 2:
     print("For example: python3 simpleinterpreter.py gcd.simple")
     sys.exit()
 filename = sys.argv[1]
-interpret(filename)
+compile(filename)
