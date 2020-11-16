67aca7a58f7b7485201e0ab4205b1baf18026153
patrickvando <patrickvando2@gmail.com>
2020-11-16 13:59:45 +0000

    removing parts that are no longer used

--- oats.py
+++ oats.py
@@ -7,7 +7,7 @@ tn.children.append(Node(Node.NUMBER_TYPE, 56))
 lexer = Lexer("test.oats")
 parser = StatementParser(lexer)
 root = parser.parse_main()
-root.print_recursive()
+#root.print_recursive()
 sym_tab = [dict()]
 interpreter = StatementInterpreter(sym_tab)
 interpreter.interpret_statement_list(root)
